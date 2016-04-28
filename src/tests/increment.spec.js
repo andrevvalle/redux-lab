069fc1bb0f95992d31aa9e707163b2ba6a071c5d
Andre Valle <andrevvalle@icloud.com>
2016-04-27 23:27:04 -0300

    Updated test increment spec

--- src/tests/increment.spec.js
+++ src/tests/increment.spec.js
@@ -10,13 +10,15 @@ describe('Increment and Drecrement Test', () => {
 	const store = createStore(ReducerIncrement);
 	let counter = store.getState();
 
+	beforeEach(() => {
+		expect(store.getState()).toEqual(0);
+	});
+
 	it('Increment and Decrement one', () => {
 		store.dispatch({ type: INCREMENT_COUNTER });
 		expect(store.getState()).toEqual(1);
 
-		afterEach(() => {
-			store.dispatch({ type: DECREMENT_COUNTER });
-		});
+		store.dispatch({ type: DECREMENT_COUNTER });
 	});
 
 	it('Increment and Decrement two', () => {
@@ -24,9 +26,11 @@ describe('Increment and Drecrement Test', () => {
 		store.dispatch({ type: INCREMENT_COUNTER });
 		expect(store.getState()).toEqual(2);
 
-		afterEach(() => {
-			store.dispatch({ type: DECREMENT_COUNTER });
-			store.dispatch({ type: DECREMENT_COUNTER });
-		});
+		store.dispatch({ type: DECREMENT_COUNTER });
+		store.dispatch({ type: DECREMENT_COUNTER });
+	});
+
+	afterEach(() => {
+		expect(store.getState()).toEqual(0);
 	});
 });
